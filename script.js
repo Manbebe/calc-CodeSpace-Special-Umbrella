2215ac94c836ab64155e0f92bdaefc33e0b2583a
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2024-12-09 13:29:00 +0000

    the cooper way

--- script.js
+++ script.js
@@ -11,3 +11,21 @@ const circleArea = (3.14 * r * 2)
 console.log(rectangleArea);
 console.log(triangleArea);
 console.log(circleArea);
+
+
+////////////or//////////////
+
+function area(shape, measure1, measure2){
+    if(shape === "rectangle"){
+      return measure1 * measure2
+    } else if(shape === "triangle"){
+         return 0.5 * measure1 * measure2;
+    } else if(shape === "circle"){
+         return Math.PI * measure1 * measure2;
+    }else{
+         return "Shape not recognized";
+    }
+}
+
+
+console.log(area("rectangle", 10, 5));
