49cf884bcba015df2c6aa2b28811c9617041ecfd
Gregory McCormack <gregorym2802@tuckahoeschools.org>
2024-12-09 13:07:54 +0000

    foundmathanswers

---
+++ script.js
@@ -0,0 +1,13 @@
+let l = 3;
+let w = 4;
+let r = 6;
+let h = 8;
+let b = 10;
+
+const rectangleArea = (1 * w)
+const triangleArea = (.5 * 1 * w)
+const circleArea = (3.14 * r * 2)
+
+console.log(rectangleArea);
+console.log(triangleArea);
+console.log(circleArea);
